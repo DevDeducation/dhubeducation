51c9850fb2b52ea1a1c9190e44df26e97668ce59
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-02-18 21:12:56 +0100

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { iSession } from "$lib";
+
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
+		interface Error {
+			code: string;
+			id: string;
+		}
+		interface Locals {
+			formData: Record<string, any>
+			userSession: iSession | undefined;
+			registered: Record<string, any> | undefined;
+		}
 		// interface PageData {}
-		// interface PageState {}
 		// interface Platform {}
 	}
 }
