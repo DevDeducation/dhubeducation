8c66eca854f490d1ff05da19b9569695ba65dc84
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-02-18 22:56:41 +0100

    Create Checkbox.svelte

---
+++ src/lib/components/widgets/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Checkbox } from "$lib/components/ui/checkbox";
+  import Label from "$lib/components/ui/label/label.svelte";
+  let checked = false;
+
+  export let label: string = ""
+  export let name: string = ""
+</script>
+
+<div class="flex items-center space-x-2">
+  <Checkbox id={name} bind:checked />
+  <Label
+    for={name}
+    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+    {label}
+  </Label>
+</div>
