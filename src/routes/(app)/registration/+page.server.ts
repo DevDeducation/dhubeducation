bd8526dadd1444153e926b3b6d722552e5a83fce
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-04-24 09:51:44 +0100

    registration link set

---
+++ src/routes/(app)/registration/+page.server.ts
@@ -0,0 +1,24 @@
+import { getMetadata, type iSlider } from "$lib";
+import type { Actions, PageServerLoad } from "./$types";
+
+
+export const load: PageServerLoad = async () => {
+  let slider: iSlider | null = null
+
+  const sliderspaths = import.meta.glob('/src/content/homepagesliders/virtual-fair.md', { eager: true })
+
+  slider = getMetadata(sliderspaths)[0] as iSlider
+
+  return { slider };
+};
+
+export const actions: Actions = {
+  default: async() => {
+    return {
+      status: 200,
+      body: {
+        message: "Hello World"
+      }
+    };
+  }
+};
---
+++ src/routes/(app)/registration/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Constants, getDescription, getOgImage, utils, type iHeroLoad, Collection } from '$lib';
+	import Coursecard from '$lib/components/cards/Coursecard.svelte';
+	import Courseform from '$lib/components/forms/Courseform.svelte';
+	import RegistrationForm from '$lib/components/forms/RegistrationForm.svelte';
+	import Hero from '$lib/components/partials/Hero.svelte'; 
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	$: heroload = data as unknown as iHeroLoad;
+	$: slider = heroload.slider;
+
+	$: title = 'Course | DHUB Education';
+	$: description = getDescription('Travel Agent Operations and Management Course');
+	$: ogimage = getOgImage('https://assets.tina.io/c33a0372-b234-4d33-889c-554ce83b13ff/popup/virtual-fair_2.webp');
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={ogimage} />
+
+	<!-- Twitter -->
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={ogimage} />
+</svelte:head>
+
+<Hero
+	options={{
+		cta: false,
+		title: slider.title,
+		subline: [slider.subline],
+		backgroundImage: slider.background
+	}}
+/>
+
+<div class="acenter">
+	<RegistrationForm />
+</div>
