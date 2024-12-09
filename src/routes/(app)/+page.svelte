ff2c9887ad3bb942815475e508ab30b2894622e9
Jumia <devdeducation@gmail.com>
2024-12-09 14:53:39 +0100

    japa

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -104,7 +104,7 @@
 	<div class="acenter">
 		<h2 class="text-xl font-semibold capitalize title text-center my-4">Sign up for January Application</h2>
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-			<div class="aspect-video rounded-lg" style={`background: url(https://www.dhubeducation.com/images/january-application.webp) no-repeat;background-size:cover;background-position:center`} />
+			<div class="aspect-video rounded-lg" style={`background: url(https://www.dhubeducation.com/japa.webp) no-repeat;background-size:cover;background-position:center`} />
  			<RegistrationForm />
 		</div>
 	</div>
